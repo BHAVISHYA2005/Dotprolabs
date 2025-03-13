b359e65ba3b5003fcd99f57e24ae4ad2ef25a611
Bhavishya Mohan Aggarwal <handballaggarwal@gmail.com>
2025-03-13 17:04:35 +0530

    fixed deployment errors , added config files and edited next.config.js

--- dotprolabs-clone/next.config.js
+++ dotprolabs-clone/next.config.js
@@ -6,28 +6,7 @@ const nextConfig = {
       "images.unsplash.com",
       "web-assets.same.dev",
     ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "web-assets.same.dev",
-        port: "",
-        pathname: "/**",
-      },
-    ],
   },
-  // Important: Remove any custom build directory settings
   output: 'standalone',
 }
 
